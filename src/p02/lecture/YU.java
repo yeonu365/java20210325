46452a4183a778f4b103082de8fd5fc82e6a792a
yeonu365 <yeonustudios@gmail.com>
2021-03-26 17:46:11 +0900

    chaptor3

--- src/p02/lecture/YU.java
+++ src/p02/lecture/YU.java
@@ -9,5 +9,18 @@ public class YU {
 		char chVar = 101;
 		int intVar = chVar;
 		System.out.println((char)intVar);
+		
+		int intValue = 'A';
+		char charValue = (char) intValue;
+		System.out.println(charValue);
+		
+		int intValue5 = 101;
+		char charValue5 = (char) intValue5;
+		System.out.println(charValue5);
+		
+		char ai = 'A';
+		int result = ai + 1;
+		char na = (char) result;
+		System.out.println(na);
 	}
 }
