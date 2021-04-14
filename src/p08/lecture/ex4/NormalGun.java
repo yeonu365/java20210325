8571b3bf643c85197647d67f106be4fcbba02604
yeonu365 <yeonustudios@gmail.com>
2021-04-14 15:49:09 +0900

    chap08

---
+++ src/p08/lecture/ex4/NormalGun.java
@@ -0,0 +1,9 @@
+package p08.lecture.ex4;
+
+public class NormalGun implements Gun {
+	@Override
+	public void fire() {
+		System.out.println("총을 쏩니다.");
+	}
+
+}
---
+++ src/p08/lecture/ex4/WaterGun.java
@@ -0,0 +1,9 @@
+package p08.lecture.ex4;
+
+public class WaterGun implements Gun {
+	@Override
+	public void fire() {
+		System.out.println("물을 쏩니다.");
+	}
+
+}
