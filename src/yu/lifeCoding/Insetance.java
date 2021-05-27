92d9017cf343d9b704555789d4805b2051567c15
yeonu365 <yeonustudios@gmail.com>
2021-05-27 16:31:18 +0900

    May

---
+++ src/yu/lifeCoding/Insetance.java
@@ -0,0 +1,24 @@
+package yu.lifeCoding;
+
+public class Insetance {
+	public static void main(String[] args) {
+		Print p1 = new Print();
+		p1.delimiter="---";
+		p1.A();
+		p1.A();
+		p1.B();
+		p1.B();
+		
+		Print p2 = new Print();
+		p2.delimiter="***";
+		p2.A();
+		p2.A();
+		p2.B();
+		p2.B();
+		
+		p1.A();
+		p2.A();
+		p1.A();
+		p2.A();
+	}
+}
---
+++ src/yu/lifeCoding/Print.java
@@ -0,0 +1,15 @@
+package yu.lifeCoding;
+
+class Print{
+	public String delimiter="";
+	public void A() {
+		System.out.println(delimiter);
+		System.out.println("A");
+		System.out.println("A");
+	}
+	public void B() {
+		System.out.println(delimiter);
+		System.out.println("B");
+		System.out.println("B");
+	}
+}
